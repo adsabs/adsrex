0b078118ef9478443567382f9a7c0f97da301a77
Roman Chyla <roman.chyla@gmail.com>
2020-06-03 21:30:47 -0400

    Fixed failing tests

--- v1_0/api/export.py
+++ v1_0/api/export.py
@@ -14,15 +14,16 @@ class ExportServiceTest(unittest.TestCase):
         data = {"bibcode": ["2018EPJWC.18612003D"], "sort": "date desc, bibcode desc",
                 "maxauthor": 2, "authorcutoff": 10, "keyformat": "%1H:%Y"}
         r = user.post('/export/bibtex', json=data)
-        self.assertTrue(r.json() == {'msg': 'Retrieved 1 abstracts, starting with number 1.',
+        self.maxDiff = None
+        self.assertEqual(r.json(), {'msg': 'Retrieved 1 abstracts, starting with number 1.',
                                      'export': '@INPROCEEDINGS{Damon:2018,\n'
                                                 '       author = {{Damon}, James and {Henneken}, Edwin and {Accomazzi}, Alberto},\n'
                                                 '        title = "{Managing Institutional Bibliographies using the ADS API: A new workflow using Google Sheets}",\n'
                                                 '    booktitle = {European Physical Journal Web of Conferences},\n'
-                                                '         year = "2018",\n'
+                                                '         year = 2018,\n'
                                                 '       series = {European Physical Journal Web of Conferences},\n'
                                                 '       volume = {186},\n'
-                                                '        month = "Jul",\n'
+                                                '        month = jul,\n'
                                                 '          eid = {12003},\n'
                                                 '        pages = {12003},\n'
                                                 '          doi = {10.1051/epjconf/201818612003},\n'
